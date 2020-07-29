45bcde9839e330d9d0c9e231fd0afa8d2e6a9c8f
Dante Falzone <dante.j.falzone@protonmail.com>
2020-07-29 16:28:54 -0500

    made test.c more complicated to test functionality

--- c-devel/test.c
+++ c-devel/test.c
@@ -13,7 +13,7 @@
 #include "lexer.h"
 
 int main(void) {
-    char *program_code = "3.14159;";
+    char *program_code = "69420   3.14159";
 
     struct lexer lex = {
         .text = program_code,
@@ -34,7 +34,22 @@ int main(void) {
         printf("It's a float!\n");
     }
 
+    printf("Skipping whitespace... ");
+    skip_whitespace(&lex);
+    printf("Whitespace skipped.\n");
+
+    struct token *second_token = parse_number(&lex);
+
+    printf("Parsed this number: %s\n", second_token->value);
+    if (second_token->type == INTEGER) {
+        printf("It's an integer!\n");
+    } else if (second_token->type == FLOAT) {
+        printf("It's a float!\n");
+    }
+
     free(first_token->value);
     free(first_token);
+    free(second_token->value);
+    free(second_token);
     return 0;
 }
