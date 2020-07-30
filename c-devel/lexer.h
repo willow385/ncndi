d9b3b68391e0d703caaa733ce5b4fb01df229df3
Dante Falzone <dante.j.falzone@protonmail.com>
2020-07-30 10:27:32 -0500

    Added enlightening documentation comments to lexer.h

--- c-devel/lexer.h
+++ c-devel/lexer.h
@@ -3,45 +3,106 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+/* Table of MPL reserved words */
 extern const char *reserved_words[];
 
+/* Number of reserved words in MPL */
 extern size_t reserved_word_count;
 
+/* The lexer is basically a simple finite state mechine. */
 struct lexer {
-    char *text;
-    size_t text_size;
-    size_t pos;
-    char current_char;
+    char *text;        // the text of the MPL source code to lex
+    size_t text_size;  // number of chars in the source code
+    size_t pos;        // index of currently selected char
+    char current_char; // currently selected char
 };
 
-/* Get the token type corresponding to an identifier.
-   If the identifier is a reserved word,  returns the
-   appropriate token_type for that word; else, simply
-   returns IDENTIFIER. */
+/**
+ * Some identifiers happen to be reserved words. If an MPL
+ * reserved word  is  passed to this function,  it returns
+ * the corresponding token_type. For example, the function
+ * call `identifier_type("funct")`  returns FUNCTION_DECL.
+ * If an identifier which is not a reserved word is passed
+ * it returns IDENTIFIER.
+ *
+ * @param identifier an MPL identifier
+ * @return corresponding token_type for that identifier
+ */
 enum token_type identifier_type(const char *identifier);
 
-/* These three functions all  mutate  lex->pos
-   in some way, but leave the other members of
-   lex alone.  */
+/**
+ * This function increments lex->pos.  If lex->pos is then
+ * equal to lex->text_size,  lex->current_char  updates to
+ * '\0';  else, lex->current_char updates to the next char
+ * in lex->text.
+ *
+ * @param lex the lexer
+ */
 void advance(struct lexer *lex);
+
+/**
+ * This function advances the lexer repeatedly  until  the
+ * current char is not a whitespace character.
+ *
+ * @param lex the lexer
+ */
 void skip_whitespace(struct lexer *lex);
+
+/**
+ * This function advances the lexer until lex->pos doesn't
+ * point inside a comment anymore.
+ *
+ * @param lex the lexer
+ */
 void skip_comment(struct lexer *lex);
 
-/* The functions  here that  return  struct token pointers
-   all use  malloc() internally. The caller is responsible
-   for freeing these. Use the function free_token(), which
-   is declared in token.h and defined in token.c,  to free
-   the structs token. */
+/**
+ * This function parses a float or  integer  literal  from
+ * the  lexer.  It returns  a  pointer to a heap-allocated
+ * struct token,  which  must be freed by the caller using
+ * the function free_token().
+ *
+ * @param lex the lexer
+ * @return integer or float literal token
+ */
 struct token *parse_number(struct lexer *lex);
+
+/**
+ * This function  parses an identifier from the lexer.  It
+ * too returns a pointer to memory that must be freed with
+ * free_token().
+ *
+ * @param lex the lexer
+ * @return identifier or reserved word token
+ */
 struct token *parse_identifier(struct lexer *lex);
+
+/**
+ * This function parses  a  string literal from the lexer.
+ * Once again,  caller must call the function free_token()
+ * on the returned pointer.
+ *
+ * @param lex the lexer
+ * @return string literal token
+ */
 struct token *parse_string_literal(struct lexer *lex);
 
-/* See what the next character is. */
+/**
+ * This function returns the upcoming character.
+ *
+ * @param lex the lexer
+ * @return lex->text[lex->pos+1] or '\0' if none left
+ */
 char peek(struct lexer *lex);
 
-/* Get the next token. This too is malloc'd and must be
-   freed  by the caller.  Once again,  remember to free
-   using free_token(). */
+/**
+ * This function returns the next token  from  the  lexer.
+ * And guess what? That's right, the caller has to free it
+ * with free_token()! Good job guessing, have a cigar.
+ *
+ * @param lex the lexer
+ * @return next token in the program
+ */
 struct token *get_next_token(struct lexer *lex);
 
 #endif // LEXER_H
--- c-devel/token.h
+++ c-devel/token.h
@@ -1,6 +1,8 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
+/* This enum is used to distinguish different
+   types of tokens. */
 enum token_type {
     MULT = 0,
     PLUS = 1,
@@ -45,12 +47,21 @@ enum token_type {
     MODULUS = 40
 };
 
-
+/* When we lex the source code, we convert it from
+   an array of char  into  an array of these guys.
+   This makes it much easier  to  parse  the  code
+   into an abstract syntax tree. */
 struct token {
     enum token_type type;
     char *value;
 };
 
+/* Whenever we create a  struct token on the heap,
+   we have to allocate memory  for token->value as
+   well as for the token itself.  So every time we
+   want to deallocate a  struct token,  we have to
+   free  token->value  *before* we can safely free
+   token itself. This function does just that. */
 void free_token(struct token *t);
 
 #endif // TOKEN_H
