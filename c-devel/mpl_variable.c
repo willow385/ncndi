e2d14320e70d3b7cf5dac24346e7cf25276e9cc5
Dante Falzone <dante.j.falzone@protonmail.com>
2020-08-10 11:56:32 -0500

    Began work on mpl_variable.c

---
+++ c-devel/mpl_variable.c
@@ -0,0 +1,34 @@
+#include "config.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "ast_node.h"
+
+
+static struct mpl_object *mpl_variable_eval(
+    struct ast_node *this_node,
+    size_t variable_count,
+    struct mpl_variable *variable_scope,
+    size_t function_count,
+    struct mpl_function *function_scope
+) {
+    MPL_DEBUG(fprintf(stderr, "DEBUG: Calling mpl_variable->eval() on mpl_variable @ %p.\n", (void *)this_node));
+    // TODO implement
+    return NULL;
+}
+
+
+static void mpl_variable_destroy_children(struct ast_node *this_node) {
+    MPL_DEBUG(fprintf(
+        stderr, "DEBUG: Calling destroy_children() methods on children of mpl_variable @ %p.\n", (void *)this_node));
+    // TODO implement
+}
+
+
+void construct_mpl_variable(
+    struct mpl_variable *dest,
+    const char *identifier,
+    struct mpl_object **value
+) {
+    MPL_DEBUG(fprintf(stderr, "DEBUG: Constructing mpl_variable @ %p.\n", (void *)dest));
+    // TODO implement
+}
