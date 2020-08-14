85969b7cbec5285e8b3d498d4771144fa5beff46
Dante Falzone <dante.j.falzone@protonmail.com>
2020-08-14 17:25:43 -0500

    Started implementing AST parser in C

---
+++ c-devel/parser.h
@@ -0,0 +1,30 @@
+#include "config.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "token.h"
+#include "lexer.h"
+#include "ast_node.h"
+
+/* The parser does not own *lex, but it does own *current_token
+   and is responsible for managing it. */
+struct parser {
+    struct lexer *lex;
+    struct token *current_token;
+    int just_parsed_program_block;
+};
+
+void eat(struct parser *parse, enum token_type type);
+
+struct mpl_program_block *program(struct parser *parse);
+
+struct ast_node *factor(struct parser *parse);
+
+struct ast_node *term(struct parser *parse);
+
+struct ast_node *math_expression(struct parser *parse);
+
+struct ast_node *expression(struct parser *parse);
+
+struct ast_node *comparison(struct parser *parse);
+
+struct mpl_program_block *parser_gen_ast(struct parser *parse);
