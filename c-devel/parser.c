c84411cae8999e02649895c294c2235d430e2e36
Dante Falzone <dante.j.falzone@protonmail.com>
2020-08-15 09:01:05 -0500

    working on parser

--- c-devel/parser.c
+++ c-devel/parser.c
@@ -13,7 +13,16 @@ struct parser {
     int just_parsed_program_block;
 };
 
-int eat(struct parser *parse, enum token_type type);
+int eat(struct parser *parse, enum token_type type) {
+    if (parse->current_token->type == type) {
+        free_token(parse->current_token);
+        parse->current_token = get_next_token(parse->lex);
+        return 0;
+    }
+
+    free_token(parse->current_token);
+    return 1;
+}
 
 struct mpl_program_block *program(struct parser *parse);
 
@@ -25,6 +34,16 @@ struct ast_node *math_expression(struct parser *parse);
 
 struct ast_node *expression(struct parser *parse);
 
-struct ast_node *comparison(struct parser *parse);
+#define IS_RELATIONAL(x)  \
+    x == LESS_THAN        \
+    || x == EQUALS        \
+    || x == GREATER_THAN  \
+    || x == NOT_EQ        \
+    || x == LESS_EQ       \
+    || x == GREATER_EQ
+
+struct ast_node *comparison(struct parser *parse) {
+    // TODO implement
+}
 
 struct mpl_program_block *parser_gen_ast(struct parser *parse);
